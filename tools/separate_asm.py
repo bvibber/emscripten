db0ee1f2ae7a0f1fe6a8d9d3e33398cb830257a4
Brion Vibber <brion@pobox.com>
2016-05-05 10:26:05 -0700

    Work in progress: support SEPARATE_ASM for modular mode
    Tweaks separate_asm.py to accept an optional modular wrapper
    constructor name. If passed, the .asm.js breakout adds a
    callback function to the constructor which adds the "asm"
    property into the Module during construction time.
    
    Still needs testing, and integration with the frontend.

--- tools/separate_asm.py
+++ tools/separate_asm.py
@@ -11,12 +11,24 @@ infile = sys.argv[1]
 asmfile = sys.argv[2]
 otherfile = sys.argv[3]
 
+if len(sys.argv) > 4:
+	modular = True
+	moduleName = sys.argv[4]
+else:
+	modular = False
+
+def asmVar(moduleVar):
+  if modular:
+    return moduleName + '["asmSetup"](' + moduleVar + ')'
+  else:
+    return moduleVar + '["asm"]'
+
 everything = open(infile).read()
 module = asm_module.AsmModule(infile).asm_js
 
 module = module[module.find('=')+1:] # strip the initial "var asm =" bit, leave just the raw module as a function
 if 'var Module' in everything:
-  everything = everything.replace(module, 'Module["asm"]')
+  everything = everything.replace(module, asmVar('Module'))
 else:
   # closure compiler removes |var Module|, we need to find the closured name
   evil = everything.find('eval(')
@@ -25,12 +37,17 @@ else:
   while everything[start] in [' ', '=']: start -= 1
   while everything[start] not in [' ', ',', '(']: start -= 1
   closured_name = everything[start+1:evil].strip()
-  everything = everything.replace(module, closured_name + '["asm"]')
+  everything = everything.replace(module, asmVar(closured_name))
 
 o = open(asmfile, 'w')
+if modular:
+  o.write(moduleName)
+  o.write('["asmSetup"] = function(Module) {')
 o.write('Module["asm"] = ')
 o.write(module)
 o.write(';')
+if modular:
+  o.write('}')
 o.close()
 
 o = open(otherfile, 'w')
